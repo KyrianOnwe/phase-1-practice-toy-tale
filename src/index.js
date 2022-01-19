0d7dd866fa0591746ea2acc9e1b3650ca3627037
Kyrian <dr.kyrian.onwe@gmail.com>
2022-01-19 12:55:08 -0600

    commit tree 6395ad429d8b00593cea709065c12f52a223d468

--- src/index.js
+++ src/index.js
@@ -13,3 +13,83 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+//fetch[GET] toys from server and..
+function getToys(){
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toyData => toyData.forEach(toy => renderToy(toy)))
+}
+
+//add NEW TOY via form
+document.querySelector('.add-toy-form').addEventListener('submit', toyUp)
+
+//handle toy card creation
+function toyUp(e){
+  e.preventDefault()
+  let newToy = {
+    name: e.target.name.value,
+    imageUrl: e.target.image.value,
+  }
+  //render the card?
+  //renderToy(newToy)
+  //post to server
+  storeToy(newToy)
+
+}
+
+//create cards and render them to the DOM
+function renderToy(toy){
+  let card = document.createElement('li')
+  card.className = 'card'
+  card.innerHTML = `
+    <div class="card">
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
+      <p>4 ${toy.likes} Likes</p>
+      <button class="like-btn" id="[toy_id]">Like ❤️</button>
+    </div>`
+
+    //Add like button functionality
+    card.querySelector('.like-btn').addEventListener('click', () => {
+      toy.likes += 1
+      card.querySelector('p').textContent = `4 ${toy.likes} Likes`
+      patchLikes(toy)
+    })
+
+    //Add toy to DOM
+    document.querySelector('#toy-collection').appendChild(card)
+}
+
+function letsGo(){
+  getToys()
+}
+
+//post to server
+function storeToy(newToy){
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers:
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+
+    body: JSON.stringify(newToy)
+  })
+  .then(res => res.json())
+  .then(toy =>renderToy(toy))
+}
+
+function patchLikes(toy){
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method:'PATCH',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify({"likes": toy.likes})
+  })
+  .then(res => res.json())
+  .then(toy => renderToy(toy))
+}
+
+letsGo()
